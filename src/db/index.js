8a99b597ce74336b3c035132185c0a16b68aac6c
Tanishk Panchal <tanineymar@users.noreply.github.com>
2026-02-03 22:33:57 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import {DB_NAME} from '../constant.js'
+
+const connectDb = async()=>{
+    try {
+      const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      console.log(`\nMongoDB connected !! DB HOSt: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MONGO DB Connection error", error);
+        process.exit(1)
+    }
+}
+
+export default connectDb
--- src/index.js
+++ src/index.js
@@ -1,2 +1,64 @@
-import express from 'express'
-const app = express()
+// require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv'
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constant";
+import connectDb from "./db/index.js";
+
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDb()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express'
+// const app = express()
+
+// (async()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error", (error)=>{
+//         console.log("ERROR" ,error)
+//         throw error
+//        })
+//        app.listen(process.env.PORT,()=>{
+//         console.log(`App is listening on port ${process.env.PORT}`)
+//        })
+//     } catch (error) {
+
+//         console.log("ERROR",error);
+//         throw err
+//     }
+// })()
