d4f9c6686511ea964c4052beaca4fb48ad7a245a
Tanishk Panchal <tanineymar@users.noreply.github.com>
2026-02-05 14:25:43 +0530

    day3 JWT tokens

---
+++ src/models/user.models.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import { JsonWebTokenError } from "jsonwebtoken";
+import bcrypt from 'bcrypt'
+
+const userSchema = new mongoose.Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowecase:true,
+            trim:true,
+            index:true,
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowecase:true,
+            trim:true,
+            
+        },
+        fullname:{
+            type:String,
+            required:true,
+            trim:true,
+            index:true
+            
+        },
+        avatar:{
+            type:String, // cloudinary url
+            required:true,
+        },
+        coverimg:{
+            type:String
+        },
+        watchHistory:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref:"Video"
+            }
+        ],
+        password:{
+            type:String,
+            required:[true, 'Password is required']
+        },
+        refreshToken:{
+            type:String
+        }
+    },
+    {timestamps:true}
+)
+
+userSchema.pre("save" ,async function(next){
+    if(!this.isModified("password"))return next()
+
+    this.password = bcrypt.hash(this.password , 10)
+   next()
+})
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password  , this.password)
+}
+export const User = mongoose.model("User" , userSchema)
