07918845ea32b16c747678fcb8d4fbf5ad444d73
Tanishk Panchal <tanineymar@users.noreply.github.com>
2026-02-13 23:35:03 +0530

    add model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema(
             trim:true,
             
         },
+        age:{
+           type:String,
+           required:true
+        },
         firstname:{
             type:String,
             required:true,
